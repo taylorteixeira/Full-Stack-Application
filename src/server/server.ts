993099e4317dbab7818b70931107b044b1b4ff8b
Taylor Vargas Teixeira <taylorvargasteixeira@gmail.com>
2024-01-12 22:32:33 -0300

    express

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,21 @@
 import express from "express";
+import os from "node:os";
 
-console.log(express);
+import config from "./config";
+
+
+const server = express();
+
+server.use(express.static("dist"));
+server.set("view engine", "ejs");
+
+server.use("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(config.PORT, config.HOST, () => {
+  console.info(
+    `Server is running on port ${config.SERVER_URL}`,
+    `free memory: ${os.freemem() / 1024 / 1024} MB`,
+  );
+});
